229d928c2121d84acb838b83bb2f7a5434189fb1
kesally <110397533+Kesally@users.noreply.github.com>
2023-06-30 10:28:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: '站点状态监控---极客青年',
+  SiteName: '枫叶--网页状态监控',
 
   // 站点链接
   SiteUrl: '/',
@@ -10,7 +10,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两种 Api Key
   ApiKeys: [
-    'ur894242-90d1eb90acc82624bcaed27a'
+    'ur2023825-ecf6e2a4005ef8381e20af58'
   ],
 
   // 是否显示监测站点的链接
@@ -24,16 +24,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: '项目主页',
-      url: 'https://github.com/geekyouth/uptime-status'
+      text: '主页',
+      url: 'https://fengye.ink/'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/geekyouth/'
+      text: 'Gitee',
+      url: 'https://gitee.com/kesally/'
     },
     {
       text: '博客',
-      url: 'https://java666.cn'
+      url: 'https://blog.fengye.ink/'
     }
   ]
 };
